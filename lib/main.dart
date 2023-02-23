d47d631ce106274cf1b065966537b5b5fa814490
faruniki <najibfahrunaakbar123@gmail.com>
2023-02-23 17:03:38 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends  StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("WhatsApp"),
+            actions: [
+              Icon(Icons.camera),
+              Icon(Icons.search),
+              Icon(Icons.line_weight_rounded)
+            ],
+            bottom: TabBar(tabs: [
+              Text("CHAT"),
+              Text("STATUS"),
+              Text("PANGGILAN")
+            ]),
+          ),
+          body: TabBarView(children: [
+            Text("Ini akan muncul di dalam tab CHATS"),
+            Text("Ini akan muncul di tab STATUS"),
+            Text("Ini akan muncul di tab PANGGILAN")
+          ],
+        ),
+        )
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override 
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      body: Center()
+    );
+  }
+}
